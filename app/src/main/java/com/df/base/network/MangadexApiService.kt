37bbc9c3c3026c1c677ace47d2752829f81def99
gabrielgtl <gabriel.sanove@gmail.com>
2024-12-08 22:33:12 -0300

    Se configura Retrofit para interactuar con la API de MangaDex y se define la interfaz MangadexApiService que nos ayudará con esto

---
+++ app/src/main/java/com/df/base/network/MangadexApiService.kt
@@ -0,0 +1,31 @@
+package com.df.base.network
+
+import com.df.base.model.mangadex.MangaList
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL =
+    "https://api.mangadex.org"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MangadexApiService {
+    @GET("/manga")
+    suspend fun getMangas(
+        @Query("title") title: String,
+        @Query("includes[]") includes: String = "cover_art"
+    ): MangaList
+}
+
+//object MangasApi {
+//   val retrofitService: MangasApiService by lazy {
+//        retrofit.create(MangasApiService::class.java)
+//    }
+//}
