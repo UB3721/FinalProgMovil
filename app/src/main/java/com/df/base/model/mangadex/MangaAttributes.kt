132e8bfa1353f05625e15d680b4c2561f1204064
gabrielgtl <gabriel.sanove@gmail.com>
2024-12-08 22:22:37 -0300

    Se agrega la clasa MangaAttributes para representar los atributos en detalle de un manga

---
+++ app/src/main/java/com/df/base/model/mangadex/MangaAttributes.kt
@@ -0,0 +1,25 @@
+package com.df.base.model.mangadex
+import kotlinx.serialization.Serializable
+
+data class MangaAttributes(
+    val title: Map<String, String>?,
+    val altTitles: List<Map<String, String>>?,
+    val description: Map<String, String>?,
+    val isLocked: Boolean?,
+    val links: Map<String, String>?,
+    val originalLanguage: String,
+    val lastVolume: String?,
+    val lastChapter: String?,
+    val publicationDemographic: String?,
+    val status: String?,
+    val year: Int?,
+    val contentRating: String?,
+    val tags: List<Tags>,
+    val state: String,
+    val chapterNumbersResetOnNewVolume: Boolean?,
+    val createdAt: String?,
+    val updatedAt: String?,
+    val version: Int?,
+    val availableTranslatedLanguages: List<String>?,
+    val latestUploadedChapter: String?,
+)
