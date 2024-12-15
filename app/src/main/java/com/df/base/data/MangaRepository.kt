d7d49faed2f6d66d9004a354fcda1d81e0515ea2
gabrielgtl <gabriel.sanove@gmail.com>
2024-12-14 23:16:08 -0300

    Se actualiza la interfaz MangasRepository para incluir nuevos métodos relacionados con la gestión de datos del usuario, estadísticas de lectura y colecciones de mangas

--- app/src/main/java/com/df/base/data/MangaRepository.kt
+++ app/src/main/java/com/df/base/data/MangaRepository.kt
@@ -1,7 +1,8 @@
-package com.df.base.data
+import com.df.base.model.back.Collection
 import com.df.base.model.back.SharedLink
 import com.df.base.model.back.User
 import com.df.base.model.back.UserManga
+import com.df.base.model.back.UserStatistics
 import com.df.base.network.SuccessResponse
 import retrofit2.Response
 interface MangasRepository {
@@ -15,5 +16,21 @@ interface MangasRepository {
 
     suspend fun getAllUsers(id: Int): List<User>
 
+    suspend fun getUserData(id: Int): User
+
     suspend fun saveSharedLink(sharedLink: SharedLink): Response<SuccessResponse>
+
+    suspend fun getAllSharedLink(id: Int): List<SharedLink>
+
+    suspend fun getUserStatistics(id: Int): UserStatistics
+
+    suspend fun saveMangaCollection(mangaId: Int, userId: Int, collectionId: Int): Response<SuccessResponse>
+
+    suspend fun deleteMangaCollection(mangaId: Int, userId: Int, collectionId: Int): Response<SuccessResponse>
+
+    suspend fun getAllCollection(id: Int): List<Collection>
+
+    suspend fun saveCollection(mangaId: Int, collectionId: Int, collectionName: String): Response<SuccessResponse>
+
+    suspend fun updateCollectionName(mangaId: Int, collectionId: Int, collectionName: String): Response<SuccessResponse>
 }
