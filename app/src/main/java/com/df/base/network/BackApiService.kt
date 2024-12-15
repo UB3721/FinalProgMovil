9a636544bbcef3d0b35cbfac76a4366ff6c9af8f
gabrielgtl <gabriel.sanove@gmail.com>
2024-12-14 23:19:25 -0300

    Se ha ampliado la interfaz BackApiService para soportar nuevas funcionalidades requeridas por el sistema

--- app/src/main/java/com/df/base/network/BackApiService.kt
+++ app/src/main/java/com/df/base/network/BackApiService.kt
@@ -1,8 +1,10 @@
 package com.df.base.network
 
+import com.df.base.model.back.Collection
 import com.df.base.model.back.SharedLink
 import com.df.base.model.back.User
 import com.df.base.model.back.UserManga
+import com.df.base.model.back.UserStatistics
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
@@ -10,6 +12,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -44,14 +47,62 @@ interface BackApiService {
         @Query("userId") userId: Int
     ): List<UserManga>
 
-    @GET("user")
+    @GET("allUsers")
     suspend fun getAllUsers(
         @Query("userId") userId: Int
     ): List<User>
 
+    @GET("user")
+    suspend fun getUserData(
+        @Query("userId") userId: Int
+    ): User
+
+    @GET("user/statistics")
+    suspend fun getUserStatistics(
+        @Query("userId") userId: Int
+    ): UserStatistics
+
     @POST("sharedLink")
     suspend fun saveSharedLink(
         @Body request: SharedLink
+    ): Response<SuccessResponse>
+
+    @GET("sharedLink")
+    suspend fun getAllSharedLink(
+        @Query("userId") userId: Int
+    ): List<SharedLink>
+
+    @POST("mangaCollection")
+    suspend fun saveMangaCollection(
+        @Query("mangaId") MangaId: Int,
+        @Query("userId") userId: Int,
+        @Query("collectionId") collectionId: Int
+    ): Response<SuccessResponse>
+
+    @DELETE("mangaCollection")
+    suspend fun deleteMangaCollection(
+        @Query("mangaId") MangaId: Int,
+        @Query("userId") userId: Int,
+        @Query("collectionId") collectionId: Int
+    ): Response<SuccessResponse>
+
+    @GET("collection")
+    suspend fun getAllCollection(
+        @Query("userId") userId: Int
+    ): List<Collection>
+
+    @POST("collection")
+    suspend fun saveCollection(
+        @Query("userId") userId: Int,
+        @Query("collectionId") collectionId: Int,
+        @Query("collectionName") collectionName: String
+    ): Response<SuccessResponse>
+
+    @PUT("collection")
+    suspend fun updateCollectionName(
+        @Query("userId") userId: Int,
+        @Query("collectionId") collectionId: Int,
+        @Query("collectionName") collectionName: String
     ): Response<SuccessResponse>
 }
 
