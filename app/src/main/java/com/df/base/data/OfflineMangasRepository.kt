ca34071b413e965e0067d428b54ef691d98dc5e6
gabrielgtl <gabriel.sanove@gmail.com>
2024-12-14 23:19:52 -0300

    La clase OfflineMangasRepository se ha actualizado para incluir métodos adicionales que extienden su funcionalidad

--- app/src/main/java/com/df/base/data/OfflineMangasRepository.kt
+++ app/src/main/java/com/df/base/data/OfflineMangasRepository.kt
@@ -1,8 +1,10 @@
 package com.df.base.data
 
+import com.df.base.model.back.Collection
 import com.df.base.model.back.SharedLink
 import com.df.base.model.back.User
 import com.df.base.model.back.UserManga
+import com.df.base.model.back.UserStatistics
 import com.df.base.network.BackApiService
 import com.df.base.network.SuccessResponse
 import retrofit2.Response
@@ -25,6 +27,21 @@ class OfflineMangasRepository(
 
     override suspend fun getAllUsers(id: Int): List<User> = apiService.getAllUsers(id)
 
+    override suspend fun getUserData(id: Int): User = apiService.getUserData(id)
+
     override suspend fun saveSharedLink(sharedLink: SharedLink): Response<SuccessResponse> = apiService.saveSharedLink(sharedLink)
 
+    override suspend fun getAllSharedLink(id: Int): List<SharedLink> = apiService.getAllSharedLink(id)
+
+    override suspend fun getUserStatistics(id: Int): UserStatistics = apiService.getUserStatistics(id)
+
+    override suspend fun saveMangaCollection(mangaId: Int, userId: Int, collectionId: Int): Response<SuccessResponse> = apiService.saveMangaCollection(mangaId, userId, collectionId)
+
+    override suspend fun deleteMangaCollection(mangaId: Int, userId: Int, collectionId: Int): Response<SuccessResponse> = apiService.deleteMangaCollection(mangaId, userId, collectionId)
+
+    override suspend fun getAllCollection(id: Int): List<Collection> = apiService.getAllCollection(id)
+
+    override suspend fun saveCollection(mangaId: Int, collectionId: Int, collectionName: String): Response<SuccessResponse> = apiService.saveCollection(mangaId, collectionId, collectionName)
+
+    override suspend fun updateCollectionName(mangaId: Int, collectionId: Int, collectionName: String): Response<SuccessResponse> = apiService.updateCollectionName(mangaId, collectionId, collectionName)
 }
